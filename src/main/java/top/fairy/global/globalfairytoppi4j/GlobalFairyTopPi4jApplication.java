dfaa9b609598c33b733677ce49b3fd4eb7faf843
jiaozg22 <jiao_zg22@163.com>
2020-08-27 15:15:44 +0800

    提交

---
+++ src/main/java/top/fairy/global/globalfairytoppi4j/GlobalFairyTopPi4jApplication.java
@@ -0,0 +1,13 @@
+package top.fairy.global.globalfairytoppi4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalFairyTopPi4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalFairyTopPi4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/fairy/global/globalfairytoppi4j/GlobalFairyTopPi4jApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fairy.global.globalfairytoppi4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalFairyTopPi4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
