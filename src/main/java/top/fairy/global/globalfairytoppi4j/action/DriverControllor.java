138728816effb825ead2f11187b6bcf944b19670
jiaozg22 <jiao_zg22@163.com>
2020-12-12 23:56:08 +0800

    麦克纳姆轮pid程序

--- src/main/java/top/fairy/global/globalfairytoppi4j/action/DriverControllor.java
+++ src/main/java/top/fairy/global/globalfairytoppi4j/action/DriverControllor.java
@@ -39,13 +39,50 @@ public class DriverControllor {
     }
 
     //这里使用@RequestMapping注解表示该方法对应的二级上下文路径
-    @RequestMapping(value = "/{MoveTypeEnum.forward.getOptKey()}" , method = RequestMethod.GET)
-    String moveType(@RequestParam(value = "speed") int speed) {
+    @RequestMapping(value = "/opt" , method = RequestMethod.POST)
+    String moveType(@RequestParam(value = "speed") int speed,@RequestParam(value = "opt") String opt) {
         logger.info(MoveTypeEnum.forward.getOptName(),"速度为：{}",speed);
-        ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
-        ControlCenter.MS42_AL_DIR_PLUS.setState(PinState.HIGH);
 
-        ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
+        new Thread(){
+            @Override
+           public void run(){
+                if(MoveTypeEnum.forward.getOptKey().equals(opt)){
+                    ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_DIR_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
+
+                    ControlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_DIR_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
+                }else if(MoveTypeEnum.left.getOptKey().equals(opt)){
+                    ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_DIR_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
+
+                    ControlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_DIR_PLUS.setState(PinState.LOW);
+                    ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
+                }else if(MoveTypeEnum.right.getOptKey().equals(opt)){
+                    ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_DIR_PLUS.setState(PinState.LOW);
+                    ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
+
+                    ControlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_DIR_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
+                }else if(MoveTypeEnum.back.getOptKey().equals(opt)){
+                    ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AL_DIR_PLUS.setState(PinState.LOW);
+                    ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
+
+                    ControlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
+                    ControlCenter.MS42_AR_DIR_PLUS.setState(PinState.LOW);
+                    ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
+                }
+           }
+        }.start();
+
+
         return "运动...";
     }
 
@@ -55,7 +92,7 @@ public class DriverControllor {
     String stop(@RequestParam(value = "stop") String stop) {
         logger.info("操作：stop");
         ControlCenter.MS42_AL_ENA_PLUS.setState(PinState.LOW);
-
+        ControlCenter.MS42_AR_ENA_PLUS.setState(PinState.LOW);
 
         return "运动...";
     }
