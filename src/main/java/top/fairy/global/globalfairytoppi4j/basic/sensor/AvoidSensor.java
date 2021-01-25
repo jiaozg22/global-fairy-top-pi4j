ad2eadfab45b1e1836afab3c371ee8fac5beb29c
jiaozg22 <jiao_zg22@163.com>
2021-01-25 23:15:57 +0800

    增加红外避障

---
+++ src/main/java/top/fairy/global/globalfairytoppi4j/basic/sensor/AvoidSensor.java
@@ -0,0 +1,33 @@
+package top.fairy.global.globalfairytoppi4j.basic.sensor;
+
+import com.pi4j.io.gpio.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author jiao_zg22
+ * @version 1.0
+ * @description 避障模块
+ *
+ * @date 2021/1/25 22:34
+ */
+public class AvoidSensor {
+    private static final Logger logger = LogManager.getLogger();
+    private static final String VCC = "1";//1号物理针脚，连接VCC针脚
+    private static final String GND = "25";//25号物理针脚，连接GND针脚
+    private static final Pin out = RaspiPin.GPIO_25;//37号物理针脚，对应GPIO_25,连接OUT针脚
+    private static GpioController gpio = GpioFactory.getInstance();
+    static GpioPinDigitalInput outReader = null;
+
+    public static void observer(){
+        //获取树莓派gpio控制模式引脚
+
+        outReader = gpio.provisionDigitalInputPin(out);
+        outReader.addListener(new AvoidSensorListener());
+    }
+
+
+    public static void close(){
+        gpio.shutdown();
+    }
+}
