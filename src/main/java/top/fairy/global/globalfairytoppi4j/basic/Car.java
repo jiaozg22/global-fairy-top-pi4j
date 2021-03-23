ba02fe0f886545511b25af7c99b7743677f88880
jiaozg22 <jiao_zg22@163.com>
2021-03-23 18:16:11 +0800

    四个轮子处理

--- src/main/java/top/fairy/global/globalfairytoppi4j/basic/Car.java
+++ src/main/java/top/fairy/global/globalfairytoppi4j/basic/Car.java
@@ -16,15 +16,14 @@ public class Car {
     private static ControlCenter controlCenter = new ControlCenter();
 
     /**
-     *
      * @description:运动
      * @author: jiao_zg22
      * @time: 2020/12/13 17:08
      */
-    public static void opt_type(int speed,String optKey){
+    public static void opt_type(int speed, String optKey) {
 
         com.pi4j.wiringpi.Gpio.wiringPiSetup();
-        if(MoveTypeEnum.forward.getOptKey().equals(optKey)){
+        if (MoveTypeEnum.forward.getOptKey().equals(optKey)) {
 
             // create soft-pwm pins (min=0 ; max=100)
 //            int setSpeedErrorNO = SoftPwm.softPwmCreate(29, 0, speed);
@@ -42,33 +41,39 @@ public class Car {
             controlCenter.MS42_BR_ENA_PLUS.setState(PinState.LOW);
             controlCenter.MS42_BR_DIR_PLUS.setState(PinState.HIGH);
 
+
+            SoftPwm.softPwmStop(PiLinkEnum.MS42_AL_PUL_PLUS.getPinNum());
+            SoftPwm.softPwmCreate(PiLinkEnum.MS42_AL_PUL_PLUS.getPinNum(), 0, speed);
+
+            SoftPwm.softPwmStop(PiLinkEnum.MS42_AR_PUL_PLUS.getPinNum());
+            SoftPwm.softPwmCreate(PiLinkEnum.MS42_AR_PUL_PLUS.getPinNum(), 0, speed);
+
+            SoftPwm.softPwmStop(PiLinkEnum.MS42_BL_PUL_PLUS.getPinNum());
+            SoftPwm.softPwmCreate(PiLinkEnum.MS42_BL_PUL_PLUS.getPinNum(), 0, speed);
+
+            SoftPwm.softPwmStop(PiLinkEnum.MS42_BR_PUL_PLUS.getPinNum());
+            SoftPwm.softPwmCreate(PiLinkEnum.MS42_BR_PUL_PLUS.getPinNum(), 0, speed);
+
             // continuous loop
             while (true) {
                 for (int i = 1; i <= speed; i++) {
 
-//                    SoftPwm.softPwmWrite(29, i);
-
-
-                    SoftPwm.softPwmStop(PiLinkEnum.MS42_AL_PUL_PLUS.getPinNum());
-                    SoftPwm.softPwmCreate(PiLinkEnum.MS42_AL_PUL_PLUS.getPinNum(), 0, speed);
                     SoftPwm.softPwmWrite(PiLinkEnum.MS42_AL_PUL_PLUS.getPinNum(), i);
 
-                    SoftPwm.softPwmStop(PiLinkEnum.MS42_AR_PUL_PLUS.getPinNum());
-                    SoftPwm.softPwmCreate(PiLinkEnum.MS42_AR_PUL_PLUS.getPinNum(), 0, speed);
                     SoftPwm.softPwmWrite(PiLinkEnum.MS42_AR_PUL_PLUS.getPinNum(), i);
 
-                    SoftPwm.softPwmStop(PiLinkEnum.MS42_BL_PUL_PLUS.getPinNum());
-                    SoftPwm.softPwmCreate(PiLinkEnum.MS42_BL_PUL_PLUS.getPinNum(), 0, speed);
                     SoftPwm.softPwmWrite(PiLinkEnum.MS42_BL_PUL_PLUS.getPinNum(), i);
 
-                    SoftPwm.softPwmStop(PiLinkEnum.MS42_BR_PUL_PLUS.getPinNum());
-                    SoftPwm.softPwmCreate(PiLinkEnum.MS42_BR_PUL_PLUS.getPinNum(), 0, speed);
                     SoftPwm.softPwmWrite(PiLinkEnum.MS42_BR_PUL_PLUS.getPinNum(), i);
 
 
-
                     try {
-                        Thread.sleep(100);
+                        long timestampBefore2 = timestamp;
+                        timestamp = System.currentTimeMillis();
+                        System.out.println(timestampBefore2 - timestamp);
+                        //50*100次 大约（有代码执行延迟）50000ms=5s后达到稳定。
+
+                        Thread.sleep(1000);//设定1s,100个脉冲。
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -83,8 +88,7 @@ public class Car {
             }
 
 
-
-        }else if(MoveTypeEnum.left.getOptKey().equals(optKey)){
+        } else if (MoveTypeEnum.left.getOptKey().equals(optKey)) {
             controlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
             controlCenter.MS42_AL_DIR_PLUS.setState(PinState.LOW);
 //            ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
@@ -92,7 +96,7 @@ public class Car {
             controlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
             controlCenter.MS42_AR_DIR_PLUS.setState(PinState.LOW);
 //            ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
-        }else if(MoveTypeEnum.right.getOptKey().equals(optKey)){
+        } else if (MoveTypeEnum.right.getOptKey().equals(optKey)) {
             controlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
             controlCenter.MS42_AL_DIR_PLUS.setState(PinState.LOW);
 //            ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
@@ -100,7 +104,7 @@ public class Car {
             controlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
             controlCenter.MS42_AR_DIR_PLUS.setState(PinState.HIGH);
 //            ControlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
-        }else if(MoveTypeEnum.back.getOptKey().equals(optKey)){
+        } else if (MoveTypeEnum.back.getOptKey().equals(optKey)) {
             controlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
             controlCenter.MS42_AL_DIR_PLUS.setState(PinState.LOW);
 //            ControlCenter.MS42_AL_PUL_PLUS.setPwmRange(speed);
@@ -127,7 +131,7 @@ public class Car {
         return "运动停止成功";
     }
 
-    public static String autoMove(int speed){
+    public static String autoMove(int speed) {
 
         controlCenter.MS42_AL_ENA_PLUS.setState(PinState.HIGH);
         controlCenter.MS42_AL_DIR_PLUS.setState(PinState.HIGH);
@@ -136,7 +140,6 @@ public class Car {
         controlCenter.MS42_AR_ENA_PLUS.setState(PinState.HIGH);
         controlCenter.MS42_AR_DIR_PLUS.setState(PinState.HIGH);
 //        controlCenter.MS42_AR_PUL_PLUS.setPwmRange(speed);
-
 
 
         //启动自动运行处理器，进行突发事件监听，并根据情况自动调用突发事件处理程序
---
+++ src/main/java/top/fairy/global/globalfairytoppi4j/utils/SoftPwmUtils.java
@@ -0,0 +1,51 @@
+package top.fairy.global.globalfairytoppi4j.utils;
+
+import com.pi4j.wiringpi.SoftPwm;
+
+
+/**
+ * @author jiao_zg22
+ * @version 1.0
+ * @description 软pwm详解
+ * @date 2021/3/23 16:09
+ */
+public class SoftPwmUtils {
+    //设置gpio针脚29号为软pwm针脚。
+
+    /**
+     * 本质上是模拟一个脉冲：1.设定一个脉冲的相对宽度（100，方便后面设置占空比的时候，更加直观）
+     * 2.设定占空比
+     * 3.设定一个脉冲的持续时间（100相对宽度，对应的实际时间，单位为毫秒）
+     * 1.SoftPwm.softPwmCreate(29, 0, 100);
+     * 2.SoftPwm.softPwmWrite(29, i);
+     * 3.Thread.Sleep(100);
+     * SoftPwm.softPwmStop(29);
+     */
+    public void move() {
+        long timestamp = 0l;
+        SoftPwm.softPwmStop(29);
+        SoftPwm.softPwmCreate(29, 0, 100);
+        try {
+            while (true) {
+                // fade LED to fully ON
+                long timestampBefore = timestamp;
+
+                timestamp = System.currentTimeMillis();
+                System.out.println(timestampBefore - timestamp);
+
+                for (int i = 1; i <= 50; i++) {//稳定后的占空比为50.
+                    SoftPwm.softPwmWrite(29, i);//为了速度上来就很高，设置一个缓冲启动
+                    long timestampBefore2 = timestamp;
+                    timestamp = System.currentTimeMillis();
+                    System.out.println(timestampBefore2 - timestamp);
+                    //50*100次 大约（有代码执行延迟）50000ms=5s后达到稳定。
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
